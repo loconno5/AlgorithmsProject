95ad0ed753a0fe44490949d7e560085b1a095684
loconno5 <loconno5@tcd.ie>
2022-04-04 14:56:14 +0100

    adding get and put methods

--- TST.java
+++ TST.java
@@ -1,4 +1,5 @@
 package AlgorithmsProject;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,254 +8,161 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import BST.Node;
+
 public class TST<Value> {
-    private int n;              // size
-    private Node<Value> root;   // root of TST
+	private int n; // size
+	private Node<Value> root; // root of TST
 
-    private static class Node<Value> {
-        private char c;                        // character
-        private Node<Value> left, mid, right;  // left, middle, and right subtries
-        private Value val;                     // value associated with string
-    }
+	private static class Node<Value> {
+		private char c; // character
+		private Node<Value> left, mid, right; // left, middle, and right subtries
+		private Value val; // value associated with string
+	}
 
-    /**
-     * Initializes an empty string symbol table.
-     */
-    public TST() {
-    }
+	/**
+	 * Initializes an empty string symbol table.
+	 */
+	public TST() {
+	}
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table
-     */
-    public int size() {
-        return n;
-    }
+	/**
+	 * Returns the number of key-value pairs in this symbol table.
+	 * 
+	 * @return the number of key-value pairs in this symbol table
+	 */
+	public int size() {
+		return n;
+	}
 
-    /**
-     * Does this symbol table contain the given key?
-     * @param key the key
-     * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public boolean contains(String key) {
-        if (key == null) {
-            throw new IllegalArgumentException("argument to contains() is null");
-        }
-        return get(key) != null;
-    }
+	/**
+	 * Does this symbol table contain the given key?
+	 * 
+	 * @param key the key
+	 * @return true if this symbol table contains {@code key} and false otherwise
+	 */
+	public boolean contains(String key) {
+		if (key == null) {
+			return false;
+		}
+		return get(key) != null;
+	}
 
-    /**
-     * Returns the value associated with the given key.
-     * @param key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public Value get(String key) {
-        if (key == null) {
-            throw new IllegalArgumentException("calls get() with null argument");
-        }
-        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-        Node<Value> x = get(root, key, 0);
-        if (x == null) return null;
-        return x.val;
-    }
+	/**
+	 * Returns the value associated with the given key.
+	 * 
+	 * @param key the key
+	 * @return the value associated with the given key if the key is in the symbol
+	 *         table and {@code null} if the key is not in the symbol table
+	 * @return null if key is null
+	 */
+	public Value get(String key) {
+		if (key == null) {
+			return null;
+		}
+		if (key.length() == 0) {
+			return null;
+		}
+		;
+		Node<Value> x = get(root, key, 0);
+		if (x == null)
+			return null;
+		return x.val;
+	}
 
-    // return subtrie corresponding to given key
-    private Node<Value> get(Node<Value> x, String key, int d) {
-        if (x == null) return null;
-        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-        char c = key.charAt(d);
-        if      (c < x.c)              return get(x.left,  key, d);
-        else if (c > x.c)              return get(x.right, key, d);
-        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
-        else                           return x;
-    }
-
-    /**
-     * Inserts the key-value pair into the symbol table, overwriting the old value
-     * with the new value if the key is already in the symbol table.
-     * If the value is {@code null}, this effectively deletes the key from the symbol table.
-     * @param key the key
-     * @param val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void put(String key, Value val) {
-        if (key == null) {
-            throw new IllegalArgumentException("calls put() with null key");
-        }
-        if (!contains(key)) n++;
-        else if(val == null) n--;       // delete existing key
-        root = put(root, key, val, 0);
-    }
-
-    private Node<Value> put(Node<Value> x, String key, Value val, int d) {
-        char c = key.charAt(d);
-        if (x == null) {
-            x = new Node<Value>();
-            x.c = c;
-        }
-        if      (c < x.c)               x.left  = put(x.left,  key, val, d);
-        else if (c > x.c)               x.right = put(x.right, key, val, d);
-        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
-        else                            x.val   = val;
-        return x;
-    }
-
-    /**
-     * Returns the string in the symbol table that is the longest prefix of {@code query},
-     * or {@code null}, if no such string.
-     * @param query the query string
-     * @return the string in the symbol table that is the longest prefix of {@code query},
-     *     or {@code null} if no such string
-     * @throws IllegalArgumentException if {@code query} is {@code null}
-     */
-    public String longestPrefixOf(String query) {
-        if (query == null) {
-            throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
-        }
-        if (query.length() == 0) return null;
-        int length = 0;
-        Node<Value> x = root;
-        int i = 0;
-        while (x != null && i < query.length()) {
-            char c = query.charAt(i);
-            if      (c < x.c) x = x.left;
-            else if (c > x.c) x = x.right;
-            else {
-                i++;
-                if (x.val != null) length = i;
-                x = x.mid;
-            }
-        }
-        return query.substring(0, length);
-    }
-
-    /**
-     * Returns all keys in the symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     * @return all keys in the symbol table as an {@code Iterable}
-     */
- /*   public Iterable<String> keys() {
-        Queue<String> queue = new Queue<String>();
-        collect(root, new StringBuilder(), queue);
-        return queue;
-    }
-
-    /**
-     * Returns all of the keys in the set that start with {@code prefix}.
-     * @param prefix the prefix
-     * @return all of the keys in the set that start with {@code prefix},
-     *     as an iterable
-     * @throws IllegalArgumentException if {@code prefix} is {@code null}
-     */
-    /*public Iterable<String> keysWithPrefix(String prefix) {
-        if (prefix == null) {
-            throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
-        }
-        Queue<String> queue = new Queue<String>();
-        Node<Value> x = get(root, prefix, 0);
-        if (x == null) return queue;
-        if (x.val != null) queue.enqueue(prefix);
-        collect(x.mid, new StringBuilder(prefix), queue);
-        return queue;
-    }
-
-    // all keys in subtrie rooted at x with given prefix
-   /* private void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
-        if (x == null) return;
-        collect(x.left,  prefix, queue);
-        if (x.val != null) queue.enqueue(prefix.toString() + x.c);
-        collect(x.mid,   prefix.append(x.c), queue);
-        prefix.deleteCharAt(prefix.length() - 1);
-        collect(x.right, prefix, queue);
-    }
-
-
-    /**
-     * Returns all of the keys in the symbol table that match {@code pattern},
-     * where the character '.' is interpreted as a wildcard character.
-     * @param pattern the pattern
-     * @return all of the keys in the symbol table that match {@code pattern},
-     *     as an iterable, where . is treated as a wildcard character.
-     */
-  /*  public Iterable<String> keysThatMatch(String pattern) {
-        Queue<String> queue = new Queue<String>();
-        collect(root, new StringBuilder(), 0, pattern, queue);
-        return queue;
-    }
- 
-    private void collect(Node<Value> x, StringBuilder prefix, int i, String pattern, Queue<String> queue) {
-        if (x == null) return;
-        char c = pattern.charAt(i);
-        if (c == '.' || c < x.c) collect(x.left, prefix, i, pattern, queue);
-        if (c == '.' || c == x.c) {
-            if (i == pattern.length() - 1 && x.val != null) queue.enqueue(prefix.toString() + x.c);
-            if (i < pattern.length() - 1) {
-                collect(x.mid, prefix.append(x.c), i+1, pattern, queue);
-                prefix.deleteCharAt(prefix.length() - 1);
-            }
-        }
-        if (c == '.' || c > x.c) collect(x.right, prefix, i, pattern, queue);
-    } */
-
+	// return subtrie corresponding to given key
+	private Node<Value> get(Node<Value> x, String key, int d) {
+		if (x == null) {
+			return null;
+		}
+		if (key.length() == 0)
+			throw new IllegalArgumentException("key must have length >= 1");
+		char c = key.charAt(d);
+		if (c < x.c) {
+			return get(x.left, key, d);
+		} else if (c > x.c) {
+			return get(x.right, key, d);
+		} else if (d < key.length() - 1) {
+			return get(x.mid, key, d + 1);
+		} else {
+			return x;
+		}
+	}
+	public void put(String key, Value val) {
+		if(!contains(key)) {
+			n++;
+			root = put(root, key, val, 0);
+		}
+	}
+	private Node<Value> put(Node<Value> x, String key, Value val, int y){
+		char c = key.charAt(y);
+		if(x ==null) {
+			x = new Node<Value>();
+			x.c = c;
+		}
+		if(c < x.c) {
+			x.left = put(x.left, key, val, y);
+		}
+		else if (c > x.c){
+			x.right = put(x.right, key, val, y);
+		}
+		else if(y < key.length()-1) {
+			x.mid = put(x.mid, key, val, (y+1));
+		}
+		else {
+			x.val = val;
+		}
+		return x;
+	}
 
 	public static void main(String[] args) {
 		ArrayList<String> busStops = new ArrayList<String>();
 		try {
-			FileReader fileReader = new FileReader("C:\\Users\\leaho\\Documents\\Alg & Data 2\\project input\\stops.txt");
+			FileReader fileReader = new FileReader(
+					"C:\\Users\\leaho\\Documents\\Alg & Data 2\\project input\\stops.txt");
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			boolean endOfFile = false;
-			while(!endOfFile) {
+			while (!endOfFile) {
 				String line = bufferedReader.readLine();
-				if(line != null) {
+				if (line != null) {
 					String[] stopData = line.split(",");
 					String stopName = stopData[2];
 					int len = stopName.length();
 					String stopNameFixed = "";
-					if(stopName.charAt(1) ==('B')) {
+					if (stopName.charAt(1) == ('B')) {
 						String temp = "";
 						temp += " ";
 						temp += stopName.charAt(0);
 						temp += stopName.charAt(1);
-						for(int i=0; i<len-2;i++) {
-							stopNameFixed += stopName.charAt(i+2);
+						for (int i = 0; i < len - 2; i++) {
+							stopNameFixed += stopName.charAt(i + 2);
 						}
 						stopNameFixed += temp;
 						busStops.add(stopNameFixed);
-					}
-					else if(stopName.contains("FLAGSTOP")) {
+					} else if (stopName.contains("FLAGSTOP")) {
 						String temp = " FLAGSTOP";
-						for(int i=0; i<len-8;i++) {
-							stopNameFixed += stopName.charAt(i+8);
+						for (int i = 0; i < len - 8; i++) {
+							stopNameFixed += stopName.charAt(i + 8);
 						}
 						stopNameFixed += temp;
 						busStops.add(stopNameFixed);
-						
-					}
-					else {
+
+					} else {
 						busStops.add(stopName);
 					}
-					
-				}
-				else {
+
+				} else {
 					endOfFile = true;
 				}
 			}
-				
-				
+
 			bufferedReader.close();
 			fileReader.close();
 
-	}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-System.out.print("first   " + busStops.get(8754));
-}
+//System.out.print("first   " + busStops.get(8754));
+	}
 }
