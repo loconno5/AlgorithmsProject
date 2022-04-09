32edd95a3b96c10460d3f24f521fb105dc7cf0aa
loconno5 <loconno5@tcd.ie>
2022-04-09 01:33:37 +0100

    adding helper classes

---
+++ EdgeWeightedDigraph.java
@@ -0,0 +1,84 @@
+package AlgorithmsProject;
+/* adapted from https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/EdgeWeightedDigraph.java */
+import java.util.NoSuchElementException;
+
+/**
+ *  The {@code EdgeWeightedDigraph} class represents a edge-weighted
+ *  digraph of vertices named 0 through <em>V</em> - 1, where each
+ *  directed edge is of type {@link DirectedEdge} and has a real-valued weight.
+ *  It supports the following two primary operations: add a directed edge
+ *  to the digraph and iterate over all of edges incident from a given vertex.
+ *  For additional documentation,
+ *  see <a href="https://algs4.cs.princeton.edu/44sp">Section 4.4</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class EdgeWeightedDigraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private final int V;                // number of vertices in this digraph
+    private int E;                      // number of edges in this digraph
+    private Bag<DirectedEdge>[] adj;    // adj[v] = adjacency list for vertex v
+    private int[] indegree;             // indegree[v] = indegree of vertex v
+    
+    /**
+     * Initializes an empty edge-weighted digraph with {@code V} vertices and 0 edges.
+     *
+     * @param  V the number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public EdgeWeightedDigraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be non-negative");
+        this.V = V;
+        this.E = 0;
+        this.indegree = new int[V];
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<DirectedEdge>();
+    }
+    /**
+     * Returns the number of vertices in this edge-weighted digraph.
+     *
+     * @return the number of vertices in this edge-weighted digraph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this edge-weighted digraph.
+     *
+     * @return the number of edges in this edge-weighted digraph
+     */
+    public int E() {
+        return E;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the directed edge {@code e} to this edge-weighted digraph.
+     *
+     * @param  e the edge
+     * @throws IllegalArgumentException unless endpoints of edge are between {@code 0}
+     *         and {@code V-1}
+     */
+    public void addEdge(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        indegree[w]++;
+        E++;
+    }
+    public Iterable<DirectedEdge> adj(int v) {
+        return adj[v];
+    }
+}
